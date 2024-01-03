efed923288afd1c0155234b735a0f3307abb4b13
mancinidsb <guilhermemancinidsb@gmail.com>
2024-01-03 14:13:32 -0300

    Problema 1001

---
+++ P1001/P1001/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace P1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine()!);
+            int B = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine($"X = {A+B}");
+        }
+    }
+}
